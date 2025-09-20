8b8f00e76bb0501a8461ea890624df1fbd7a6650
leonyi04 <nyilampang@gmail.com>
2025-09-21 04:23:00 +0700

    location data for reliefs

--- src/pages/Disasters/DisasterCard.jsx
+++ src/pages/Disasters/DisasterCard.jsx
@@ -12,7 +12,7 @@ const DisasterCard = (props) => {
     <Link to={`/donations/${props.data.id}`} className='min-w-[200px] max-w-[200px] min-h-[250px] max-h-[250px] bg-secondary shadow-md'>
         <img className="w-full h-40 rounded-md " src={props.data.image} alt={props.data.name} />
         <h3 className='text-sm uppercase font-semibold text-gray-800 h-10 '>{props.data.name}</h3>
-        <div className="h-4 my-2 bg-secondary rounded-full">
+        <div className="h-4 my-2 bg-background rounded-full">
           <div className="h-full bg-primary rounded-full" style={{ width: `${progress}%` }}></div>
           <div className="text-xs text-gray-800 mt-1">
             ${props.data.raised?.toLocaleString() || 0} raised of ${props.data.goal?.toLocaleString() || 0}
