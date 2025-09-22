18b008243c6c4b2d631cd5f85210964e655eecdd
leonyi04 <nyilampang@gmail.com>
2025-09-22 12:07:12 +0700

    restructured orgs&sponsors and donations&volunteers

--- src/pages/DonationsAndVolunteer/Volunteer/Volunteers.jsx
+++ src/pages/DonationsAndVolunteer/Volunteer/Volunteers.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import data from "../../assets/test_data.json";
+import data from "../../../assets/test_data.json";
 
 const Volunteers = () => {
   const volunteers = data.volunteers;
